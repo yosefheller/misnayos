071e3eb399ec15a7b6f3a87a30cc262fa21d56de
Yosef Heller <yosefmheller.com>
2019-07-08 17:24:02 -0400

    DBInfo

--- myapp/models/db_connection.js
+++ myapp/models/db_connection.js
@@ -1,16 +1,21 @@
 const Sequelize = require("sequelize");
-const sequelize = new Sequelize("mishnayos_project", "root", "password", {
-  host: "localhost",
-  dialect: "mysql",
-  operatorsAliases: false,
+const sequelize = new Sequelize(
+  "heroku_37d590bbeb349e8",
+  "bbfbf9e27f94a5",
+  "5c7f2f36",
+  {
+    host: "us-cdbr-iron-east-02.cleardb.net",
+    dialect: "mysql",
+    operatorsAliases: false,
 
-  pool: {
-    max: 5,
-    min: 0,
-    acquire: 30000,
-    idle: 10000
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000
+    }
   }
-});
+);
 sequelize
   .authenticate()
   .then(() => {
